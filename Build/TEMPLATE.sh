46d8c67467d735291f6b69f001c1c61e22e1e2a9
Will Simpson <will@kestrelcreek.com>
2023-01-15 14:03:13 -0800

    Modified directories

--- Build/TEMPLATE.sh
+++ Build/TEMPLATE.sh
@@ -1,15 +1,15 @@
 #!/bin/zsh
 
-echo "What is your essay's working title?"
-read NAME
-mkdir /Users/will/Dropbox/Projects/Writing/Active/$NAME
-cd /Users/will/Dropbox/Projects/Writing/Active/$NAME
+# echo "What is your essay's working title?"
+# read NAME
+mkdir /Users/will/Dropbox/Projects/Writing/Active/$1
+cd /Users/will/Dropbox/Projects/Writing/Active/$1
 mkdir _drafts
 mkdir _research
 mkdir _images
 mkdir _stage
 # cp /Users/will/Dropbox/zettelkasten/Writing/Tools/TEMPLATE.sh .
-cp /Users/will/Dropbox/Projects/Writing/Build/Templates/template.md $NAME.md
+# cp /Users/will/Dropbox/Projects/Writing/Build/Templates/template.md $1.md
 # cp /Users/will/Dropbox/zettelkasten/Writing/Tools/mla.csl .
 touch README.markdown
 # touch scratch-$NAME.md
